65ec846238db9564d7201ef426f99d162a312d4b
bnguyen-nanolos <thienphamuser@gmail.com>
2023-03-09 20:51:06 +0700

    create initial files

---
+++ ShoesShop/src/main/java/shoesShop/ShoesShopApplication.java
@@ -0,0 +1,13 @@
+package shoesShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesShopApplication.class, args);
+	}
+
+}
