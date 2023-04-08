e70b3753ca73b4e22760856e368961f17491eaa3
bnguyen-nanolos <thienphamuser@gmail.com>
2023-04-08 17:14:22 +0700

    Add emuns

--- ShoesShop/src/main/java/shoesShop/enums/OrderStatus.java
+++ ShoesShop/src/main/java/shoesShop/enums/OrderStatus.java
@@ -1,5 +1,21 @@
 package shoesShop.enums;
 
 public enum OrderStatus {
+	Cancelled("Cancelled"),
+	Completed("Completed"),
+	Failed("Failed"),
+	OnHold("OnHold"),
+	PendingPayment("PendingPayment"),
+	Processing("Processing"),
+	Refunded("Refunded");
 	
+	private String description;
+	
+	OrderStatus(String description) {
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
 }
--- ShoesShop/src/main/java/shoesShop/enums/ProductStatus.java
+++ ShoesShop/src/main/java/shoesShop/enums/ProductStatus.java
@@ -1,5 +1,20 @@
 package shoesShop.enums;
 
 public enum ProductStatus {
+	// The product is ready to sell and is available to customers 
+	Active("Active"),
+	// The product is no longer being sold and isn't available to customers
+	Archived("Archived"),
+	// The product isn't ready to sell and is unavailable to customers
+	Draft("Draft");
 	
+	private String description;
+	
+	ProductStatus(String description) {
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
 }
