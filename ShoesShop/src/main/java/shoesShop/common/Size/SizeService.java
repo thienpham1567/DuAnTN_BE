d94005db650819e7fce9afb580fe96eaf46cf25f
dtrung112 <85286528+ldt112@users.noreply.github.com>
2023-06-16 10:53:44 +0700

    size

--- ShoesShop/src/main/java/shoesShop/common/Size/SizeService.java
+++ ShoesShop/src/main/java/shoesShop/common/Size/SizeService.java
@@ -1,34 +1,75 @@
-//package shoesShop.common.Size;
-//
-//import java.util.Collection;
-//import java.util.stream.Collectors;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import shoesShop.common.RecordManager;
-//
-//@Service
-//public class SizeService extends RecordManager<Size> {
-//	@Autowired
-//	ISizeRepository sizeRepo;
-//
-//	SizeConverter converter = new SizeConverter();
-//
-//	@Override
-//	public Collection<Size> retrieveAll() {
-//		Collection<Size> sizes = this.load(null).stream().map(dbSize -> this.converter.convertDbToModel(dbSize))
-//				.collect(Collectors.toList());
-//		return sizes;
-//	}
-//
-//	private Collection<DbSize> load(Integer sizeId) {
-//		Collection<DbSize> dbSizes = this.sizeRepo.findAll();
-//
-//		if (sizeId != null) {
-//			dbSizes = dbSizes.stream().filter(dbSize -> dbSize.sizeId == sizeId).collect(Collectors.toList());
-//		}
-//
-//		return dbSizes;
-//	}
-//}
+package shoesShop.common.Size;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import shoesShop.common.RecordManager;
+import shoesShop.common.Category.ICategoryRepository;
+
+@Service
+public class SizeService extends RecordManager<Size> {
+	@Autowired
+	private ISizeRepository sizeRepo;
+
+	SizeConverter converter = new SizeConverter();
+	@Autowired
+	private ICategoryRepository categoryRepo;
+
+	@Override
+	public Collection<Size> retrieveAll() {
+		Collection<Size> sizes = this.load(null).stream().map(dbSize -> this.converter.convertDbToModel(dbSize))
+				.collect(Collectors.toList());
+		return sizes;
+	}
+	
+	@Override
+	public Size retrieveOne(Integer id) throws Exception {
+		Size size = this.load(id).stream().map(dbSize -> this.converter.convertDbToModel(dbSize)).findFirst().get();
+		return size;
+	}
+	
+	@Override
+	public Size create(Size size) throws Exception {
+		System.out.println(size.getValue());
+		System.out.println(size.getCategoryId());
+		
+		DbSize dbSize = this.converter.convertModelToDb(size);
+		
+		dbSize.category = this.categoryRepo.findById(size.categoryId).get();
+		DbSize createdSize = this.sizeRepo.save(dbSize);
+		
+		return this.converter.convertDbToModel(createdSize);
+	}
+
+	@Override
+	public Size update(Size size, Integer id) throws Exception {
+		DbSize updateSize = this.converter.convertModelToDb(size);
+		DbSize dbSize = this.sizeRepo.findById(id).get();
+		if (dbSize != null) {
+			this.converter.combine(dbSize, updateSize);
+			DbSize updateDbSize = this.sizeRepo.save(dbSize);
+			return this.converter.convertDbToModel(updateDbSize);
+		}
+		return size;
+	}
+	
+	@Override
+	public Boolean delete(Integer id) throws Exception {
+		if (this.sizeRepo.existsById(id)) {
+			this.sizeRepo.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+	private Collection<DbSize> load(Integer sizeId) {
+		Collection<DbSize> dbSizes = this.sizeRepo.findAll();
+		if (sizeId != null) {
+			dbSizes = dbSizes.stream().filter(dbSize -> dbSize.sizeId == sizeId).collect(Collectors.toList());
+		}
+		return dbSizes;
+	}
+}
--- ShoesShop/src/main/java/shoesShop/controller/SizeController.java
+++ ShoesShop/src/main/java/shoesShop/controller/SizeController.java
@@ -1,27 +1,73 @@
-//package shoesShop.controller;
-//
-//import java.util.Collection;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import shoesShop.common.Size.Size;
-//import shoesShop.common.Size.SizeService;
-//
-//@RestController
-//@RequestMapping("api/v1/sizes")
-//public class SizeController {	
-//	@Autowired
-//	SizeService sizeService;
-//	
-//	@GetMapping
-//	public ResponseEntity<Collection<Size>> retrieveAll() {
-//		Collection<Size> sizes = this.sizeService.retrieveAll();
-//		
-//		return new ResponseEntity<Collection<Size>>(sizes, HttpStatus.OK);
-//	}
-//}
+package shoesShop.controller;
+
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import shoesShop.common.Size.Size;
+import shoesShop.common.Size.SizeService;
+
+@RestController
+@RequestMapping("api/v1/sizes")
+public class SizeController {	
+	@Autowired
+	SizeService sizeService;
+	
+	@GetMapping
+	public ResponseEntity<Collection<Size>> retrieveAll() {
+		Collection<Size> sizes = this.sizeService.retrieveAll();
+		
+		return new ResponseEntity<Collection<Size>>(sizes, HttpStatus.OK);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Size> retrieveOne(@PathVariable("id") Integer id) throws Exception {
+		Size size = sizeService.retrieveOne(id);
+		if (size != null) {
+			return new ResponseEntity<Size>(size, HttpStatus.OK);
+		}
+
+		return new ResponseEntity<Size>(HttpStatus.NOT_FOUND);
+	}
+
+	@PostMapping
+	public ResponseEntity<Size> create(@RequestBody @Valid Size size, BindingResult result) throws Exception {
+		if (size == null || result.hasErrors()) {
+			return new ResponseEntity<Size>(HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity<Size>(this.sizeService.create(size), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Size> update(@RequestBody @Valid Size size,@PathVariable("id") Integer id, BindingResult result) throws Exception {
+		if (size == null || result.hasErrors()) {
+			return new ResponseEntity<Size>(HttpStatus.BAD_REQUEST);
+		}
+		
+		Size updatedSize = this.sizeService.update(size, id);
+
+		return updatedSize != null
+				? new ResponseEntity<Size>(updatedSize, HttpStatus.OK)
+				: new ResponseEntity<Size>(HttpStatus.BAD_REQUEST);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<Size> delete(@PathVariable("id") Integer id) throws Exception {
+		return this.sizeService.delete(id) ? new ResponseEntity<Size>(HttpStatus.NO_CONTENT) : new ResponseEntity<Size>(HttpStatus.BAD_REQUEST);
+	}
+
+}
