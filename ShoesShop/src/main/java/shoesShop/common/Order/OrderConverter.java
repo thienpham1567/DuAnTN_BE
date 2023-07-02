53e3cf4eb89138e5677dc3f6a5cfbe118142b49b
Tetsu <87410424+thanh1062@users.noreply.github.com>
2023-07-02 09:53:15 +0700

    Order

--- ShoesShop/src/main/java/shoesShop/common/Order/OrderConverter.java
+++ ShoesShop/src/main/java/shoesShop/common/Order/OrderConverter.java
@@ -1,40 +1,43 @@
-//package shoesShop.common.Order;
-//
-//import shoesShop.common.ICombiner;
-//import shoesShop.common.IConverter;
-//
-//public class OrderConverter  implements ICombiner<DbOrder>, IConverter<DbOrder, Order> {
-//	@Override
-//	public void combine(DbOrder original, DbOrder update) {
-//		original.user = update.user;
-//		original.denormalizedAddress = update.denormalizedAddress;
-//		original.orderTotalPrice = update.orderTotalPrice;
-//		original.ordersStatus = update.ordersStatus;
-//		original.orderStatus = update.orderStatus;
-//		original.updatedAt = update.updatedAt;
-//	}
-//
-//	@Override
-//	public DbOrder convertModelToDb(Order input) {
-//		return input == null ? null : new DbOrder(
-//					input.denormalizedAddress,
-//					input.orderTotalPrice,
-//					input.orderAddress,
-//					input.ordersStatus,
-//					input.orderStatus
-//				);
-//	}
-//
-//	@Override
-//	public Order convertDbToModel(DbOrder input) {
-//		return input == null ? null : new Order(
-//					input.denormalizedAddress,
-//					input.orderTotalPrice,
-//					input.orderAddress,
-//					input.ordersStatus,
-//					input.orderStatus,
-//					input.orderId,
-//					input.user.userId
-//			);
-//	}
-//}
+package shoesShop.common.Order;
+
+import shoesShop.common.ICombiner;
+import shoesShop.common.IConverter;
+import shoesShop.common.User.UserConverter;
+
+public class OrderConverter  implements ICombiner<DbOrder>, IConverter<DbOrder, Order> {
+	private UserConverter userConverter = new UserConverter();
+	
+	@Override
+	public void combine(DbOrder original, DbOrder update) {
+		original.user = update.user;
+		original.denormalizedAddress = update.denormalizedAddress;
+		original.orderTotalPrice = update.orderTotalPrice;
+		original.ordersStatus = update.ordersStatus;
+		original.createdAt = update.createdAt;
+		original.updatedAt = update.updatedAt;
+	}
+
+	@Override
+	public DbOrder convertModelToDb(Order input) {
+		return input == null ? null : new DbOrder(
+					input.denormalizedAddress,
+					input.orderTotalPrice,
+					input.ordersStatus,
+					input.createdAt,
+					input.updatedAt
+				);
+	}
+
+	@Override
+	public Order convertDbToModel(DbOrder input) {
+		return input == null ? null : new Order(
+					input.denormalizedAddress,
+					input.orderTotalPrice,
+					input.ordersStatus,
+					input.createdAt,
+					input.updatedAt,
+					input.orderId,
+					this.userConverter.convertDbToModel(input.user)
+			);
+	}
+}
