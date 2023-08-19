00c5cfa538dea5ca6dd682432ffa13eb998b9a31
bnguyen-nanolos <86146840+thienpham1567@users.noreply.github.com>
2023-08-19 16:21:34 +0700

    up

--- ShoesShop/src/main/java/shoesShop/common/Order/IOrderRepository.java
+++ ShoesShop/src/main/java/shoesShop/common/Order/IOrderRepository.java
@@ -1,6 +1,8 @@
 package shoesShop.common.Order;
 
 import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
